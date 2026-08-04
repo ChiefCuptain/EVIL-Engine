65dfc308df797f6ff79046d07374a69f717f21b1
Cup <121146166+ChiefCuptain@users.noreply.github.com>
2026-08-04 09:21:02 -0600

    Replaced raw pointers in Main.cpp with smart pointers

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,58 +3,73 @@
 #include <map>
 #include <memory>
 
-//class Object
-//{
-//public:
-//    Object() { std::cout << "Constructor\n"; }
-//    ~Object() { std::cout << "Destructor\n"; }
-//
-//    Object(const Object& object) { std::cout << "Copy constructor\n"; }
-//    Object& operator = (const Object& object) { std::cout << "Assignment operator \n"; return *this; }
-//
-//};
+class Object
+{
+public:
+    Object() { std::cout << "Constructor\n"; }
+    ~Object() { std::cout << "Destructor\n"; }
+
+    Object(const Object& object) { std::cout << "Copy constructor\n"; }
+    Object& operator = (const Object& object) { std::cout << "Assignment operator \n"; return *this; }
+
+};
 
 int main()
 {
 
-    //std::cout << "===============Object===============\n";
-    //{
-    //    Object objectA;
-    //    Object objectB = objectA;
-    //    Object objectC;
-    //    objectC = objectA;
-    //}
+    std::cout << "===============Object===============\n";
+    {
+        Object objectA;
+        Object objectB = objectA;
+        Object objectC;
+        objectC = objectA;
+    }
 
-    //std::cout << "===============Raw Pointers===============\n";
-    //{
-    //    Object* objectA = new Object();
-    //    std::cout << objectA << "\n";
-    //    Object* objectB = new Object(*objectA);
-    //    std::cout << objectB << "\n";
+    std::cout << "===============Raw Pointers===============\n";
+    {
+        Object* objectA = new Object();
+        std::cout << objectA << "\n";
+        Object* objectB = new Object(*objectA);
+        std::cout << objectB << "\n";
 
-    //    Object* objectC = nullptr;
-    //    objectC = objectA;
-    //    std::cout << objectC << "\n";
+        Object* objectC = nullptr;
+        objectC = objectA;
+        std::cout << objectC << "\n";
 
-    //    delete objectA;
-    //    delete objectB;
-    //    //delete objectC;
-    //}
+        delete objectA;
+        delete objectB;
+        //delete objectC;
+    }
 
-    //std::cout << "===============Smart Pointers===============\n";
-    //{
-    //    std::unique_ptr<Object> objectA = std::make_unique<Object>();
-    //    std::cout << objectA.get() << "\n";
-    //    std::unique_ptr<Object> objectB;
-    //    objectB = std::move(objectA);
-    //    std::cout << objectA.get() << "\n";
-    //    std::cout << objectB.get() << "\n";
-
-
-    //}
+    std::cout << "===============Unique Pointers===============\n";
+    {
+        std::unique_ptr<Object> objectA = std::make_unique<Object>();
+        std::cout << objectA.get() << "\n";
+        std::unique_ptr<Object> objectB;
+        objectB = std::move(objectA);
+        std::cout << objectA.get() << "\n";
+        std::cout << objectB.get() << "\n";
 
 
-    //return 0;
+    }
+
+    std::cout << "===============Shared Pointers===============\n";
+    std::shared_ptr<Object> objectC;
+    {
+        auto objectA = std::make_shared<Object>();
+        std::cout << objectA.get() << "\n";
+        std::cout << objectA.use_count() << "\n";
+        auto objectB = objectA;
+        std::cout << objectB.get() << "\n";
+        std::cout << objectB.use_count() << "\n";
+        objectC = objectA;
+        std::cout << objectC.get() << "\n";
+        std::cout << objectC.use_count() << "\n";
+    }
+    std::cout << objectC.use_count() << "\n";
+
+
+    return 0;
 
     // INITIALIZATION
     nu::Engine::Get().Initialize();
